390fb04c808367bc9e8c48119d337762119960cc
ankush <ankushsolanki3287@gmail.com>
2022-08-31 13:06:46 +0530

    Added login and dashboard routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
@@ -21,6 +22,12 @@ if (process.env.NODE_ENV === "development") {
 //Setting Handlebars
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+//Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 3000;
 
